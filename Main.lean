f3f9f134f6cb4e4142231c6b812ca4187206e1e6
nonor <nono.rousset@gmail.com>
2025-10-06 09:37:26 +0000

    first

--- Main.lean
+++ Main.lean
@@ -1,4 +1,9 @@
-import DocTest
-
+-- The `main` function is the entry point of your program.
+-- Its type is `IO Unit` because it can perform `IO` operations (side effects).
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  -- Define a list of names
+  let names := ["One", "Two", "Three"]
+
+  -- Print the list of greetings
+  for name in names do
+    IO.println name

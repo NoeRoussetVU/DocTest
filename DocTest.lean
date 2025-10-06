5ae92db4262cee85077fc0cf83f6ee34af4740e4
nonor <nono.rousset@gmail.com>
2025-10-06 18:00:10 +0000

    add mathlib

--- DocTest.lean
+++ DocTest.lean
@@ -1,3 +1,18 @@
 -- This module serves as the root of the `DocTest` library.
 -- Import modules here that should be built as part of the library.
 import DocTest.Basic
+import Mathlib.Computability.DFA
+
+/-!
+
+  # Nothing interesting
+
+-/
+
+/-- this is very interesting-/
+def what : DFA Nat Nat:=
+{
+  step := fun x y => y
+  start := 0
+  accept := {x | x=100}
+}
